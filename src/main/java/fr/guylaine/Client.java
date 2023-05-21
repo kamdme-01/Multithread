9f975866c0b605dc96276c268016c6bc37e7dfcf
kamdme-01 <kamdemguylaine20@gmail.com>
2023-05-21 22:30:20 +0200

    creation of class

--- src/main/java/fr/guylaine/Client.java
+++ src/main/java/fr/guylaine/Client.java
@@ -5,77 +5,94 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+
 public class Client {
-	private static final String String = null;
+	private static final String string = null;
 	private Socket socket;
 	private BufferedReader bufferedReader;
 	private BufferedWriter bufferedWriter;
 	private String username;
-	
-	public Client (Socket socket,  String username) {
+
+	public Client(Socket socket, String username) {
 		try {
 			this.socket = socket;
-			this.bufferedWriter = new BufferedWriter (new OutputStreamWriter(socket.getOutputStream()));
-			this.bufferedReader = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-			this.username = username ;
-			} catch (IOException e) {
-				closeEverything (socket, bufferedReader, bufferedWriter);
-			}
-	}
-
-
-public void sendMessage () {
-		try {
-				bufferedWriter.write(username);
-				bufferedWriter.newLine();
-				bufferedWriter.flush();
-			
-				Scanner scanner = new Scanner(System.in);
-			while (socket.isConnected()) {
-				String messageToSend = scanner.nextLine();
-				bufferedWriter.write( username + " : " + messageToSend);
-				bufferedWriter.newLine();
-				bufferedWriter.flush();
-			}
-		}catch (IOException e) {
+			this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			this.username = username;
+		} catch (IOException e) {
 			closeEverything(socket, bufferedReader, bufferedWriter);
 		}
-}
+	}
 
-public void listenForMessage() {
-	new Thread(new Runnable() {
-		
-		public void run() {
-			String msgFromGroupChat;
-			while(socket.isConnected()) {
-				try {
-					msgFromGroupChat = bufferedReader.readLine();
-					System.out.println(msgFromGroupChat);
-				} catch (IOException e) {
-					closeEverything(socket, bufferedReader, bufferedWriter);
+	// ** changement: c'est cette classe qui permet d'inverser le message
+	String reverse(String str) {
+		if ((null == str) || (str.length() <= 1)) {
+			return str;
+		}
+		return reverse(str.substring(1)) + str.charAt(0);
+	}
+
+	public void sendMessage() {
+		try {
+			bufferedWriter.write(username);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+
+			Scanner scanner = new Scanner(System.in);
+			while (socket.isConnected()) {
+				String messageToSend = scanner.nextLine();
+				String messageReverse = reverse(messageToSend);
+				bufferedWriter.write(username + " : " + messageToSend.length() + " " + messageReverse);
+				bufferedWriter.newLine();
+				bufferedWriter.flush();
+			}
+			scanner.close();
+		} catch (IOException e) {
+			closeEverything(socket, bufferedReader, bufferedWriter);
+		}
+	}
+
+	public void listenForMessage() {
+		new Thread(new Runnable() {
+
+			public void run() {
+				String msgFromGroupChat;
+				while (socket.isConnected()) {
+					try {
+						msgFromGroupChat = bufferedReader.readLine();
+						System.out.println(msgFromGroupChat);
+					} catch (IOException e) {
+						closeEverything(socket, bufferedReader, bufferedWriter);
+					}
 				}
 			}
-		}
-	}).start();
-}
-
-public void closeEverything (Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
-	try {
-		if (bufferedReader != null) {
-			bufferedReader.close();
-		}
-		if (bufferedWriter != null) {
-			bufferedWriter.close();
-		}
-		if (socket != null) {
-			socket.close();
-		}
-	}catch (IOException e) {
-		e.printStackTrace();
-	}
+		}).start();
 	}
 
-public static void main (String[] args) throws UnknownHostException, IOException {
-
+	public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+		try {
+			if (bufferedReader != null) {
+				bufferedReader.close();
+			}
+			if (bufferedWriter != null) {
+				bufferedWriter.close();
+			}
+			if (socket != null) {
+				socket.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	public static void main(String args[]) throws UnknownHostException, IOException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your username for the group chat: ");
+		String username = scanner.nextLine();
+		Socket socket = new Socket("localhost", Integer.parseInt(args[0]));
+		Client client = new Client(socket, username);
+		client.listenForMessage();
+		client.sendMessage();
+		scanner.close();
+	}
+
 }
--- src/main/java/fr/guylaine/Server.java
+++ src/main/java/fr/guylaine/Server.java
@@ -5,34 +5,41 @@ import java.net.Socket;
 
 public class Server {
 	private ServerSocket serverSocket;
-	public Server (ServerSocket serverSocket) {
+
+	public Server(ServerSocket serverSocket) {
 		this.serverSocket = serverSocket;
-		
+
 	}
-	
+
 	public void startServer() {
-		
+
 		try {
 			while (!serverSocket.isClosed()) {
-				
-				Socket socket = serverSocket.accept(); 
+
+				Socket socket = serverSocket.accept();
 				System.out.println("A new client has connected");
-				ClientHandler clientHandler = new ClientHandler (socket);
-				
-				Thread thread = new Thread (clientHandler);
+				ClientHandler clientHandler = new ClientHandler(socket);
+
+				Thread thread = new Thread(clientHandler);
 				thread.start();
-				}
+			}
 		} catch (IOException e) {
+		}
 	}
-}
+
 	public void closeServerSocket() {
-		try{
+		try {
 			if (serverSocket != null) {
 				serverSocket.close();
-				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-}
-
+	}
+	public static void main(String args[]) throws IOException {
+		// the server port is passed as parameter
+		ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+		Server server = new Server(serverSocket);
+		server.startServer();
+	}
 }
